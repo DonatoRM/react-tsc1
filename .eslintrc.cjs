3e0fb397ee7a86e1cb98f4d31f6836938db49f48
DonatoRM <donatoramos@hotmail.com>
2023-09-03 14:37:29 +0200

    ci: initial conditions

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2020: true },
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/strict-type-checked',
+		'plugin:@typescript-eslint/stylistic-type-checked',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'prettier'
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['./tsconfig.json', './tsconfig.node.json'],
+		tsconfigRootDir: __dirname
+	},
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true }
+		],
+		'react/prop-types': 'off',
+		'@typescript-eslint/no-unnecessary-condition': 'off'
+	}
+};
---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+const App = () => <img src='/react.svg' alt='logotipo' />;
+
+export default App;
