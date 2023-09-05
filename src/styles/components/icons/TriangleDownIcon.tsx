cc9e6c355eb2553b7d84a59f44c7650b21c37dcc
DonatoRM <donatoramos@hotmail.com>
2023-09-05 13:03:01 +0200

    feat: create TriangleDownIcon

---
+++ src/styles/components/icons/TriangleDownIcon.tsx
@@ -0,0 +1,17 @@
+interface TriangleDownIconProps {
+	className: string;
+}
+
+const TriangleDownIcon = (props: TriangleDownIconProps) => (
+	<svg
+		{...props}
+		viewBox='0 0 24 24'
+		fill='currentColor'
+		height='1em'
+		width='1em'
+	>
+		<path d='M11.646 15.146L5.854 9.354a.5.5 0 01.353-.854h11.586a.5.5 0 01.353.854l-5.793 5.792a.5.5 0 01-.707 0z' />
+	</svg>
+);
+
+export default TriangleDownIcon;
